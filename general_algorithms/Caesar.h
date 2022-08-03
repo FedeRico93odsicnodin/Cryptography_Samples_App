e4d07e5a270b9e12b9270bb9c7c525d317f3b830
federico <federico.nigro93@gmail.com>
2022-08-03 12:52:27 +0200

    completing the Caesar algorithm TODO: 1) fixing the decryption part (the inverse) 2) list of all the other algorithms to include

--- general_algorithms/Caesar.h
+++ general_algorithms/Caesar.h
@@ -7,9 +7,15 @@
 #include <gtk/gtkx.h>
 #include <math.h>
 #include <ctype.h>
-
-
 #include "../utils_form_encrypt.h"
+
+
+////////// CONST STRINGS FOR THE FORM //////////
+const char *LBL_TXT_1_ENC = "please insert the text to encrypt";
+const char *LBL_TXT_2_DEC = "please insert the text to decrypt";
+const char *LBL_BTN_1_ENC = "encrypt!";
+const char *LBL_BTN_2_DEC = "decrypt!";
+
 // the builder for the view
 GtkBuilder *_builder;
 
@@ -23,6 +29,10 @@ GtkTextView *encr_txt;
 GtkTextView *decr_txt;
 GtkTextView *key_length;
 
+// label and button for decription updates
+GtkLabel *descr_txt_label;
+GtkButton *action_btn;
+
 
 
 // window for the algorithm initialization 
@@ -35,20 +45,29 @@ void InitMainWindow_Caesar(GtkWidget *win_caesar, GtkBuilder *builder) {
     encr_txt = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "text_to_encrypt"));
     decr_txt = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "encrypted_text"));
     key_length = GTK_TEXT_VIEW(gtk_builder_get_object(builder, "key_length"));
+
+    // getting the change text elements 
+    descr_txt_label = GTK_LABEL(gtk_builder_get_object(builder, "txt_1_descr_lbl"));
+    action_btn = GTK_BUTTON(gtk_builder_get_object(builder, "enc_action_btn"));
+    
 }
 
 ////////// ALGORITHM USED FOR THE ENCRYPTION //////////
 
 gchar *encrypt_caesar(gchar *txtToEncrypt, int key) {
 
-    gchar *s;
     int txt_len = strlen(txtToEncrypt);
-    s = "aaa";
-
     for (int i = 0; i < txt_len; i++) {
 
         char ch;
-        ch = txtToEncrypt[i] + key;
+        // decision for encrypt / decrypt 
+        if(!_isDecryption) {
+            ch = txtToEncrypt[i] + key;
+        }
+        else {
+            ch = txtToEncrypt[i] - key;
+        }
+           
         txtToEncrypt[i] = ch;
     }
     // done encryption 
@@ -59,7 +78,7 @@ gchar *encrypt_caesar(gchar *txtToEncrypt, int key) {
 
 ////////////////////////////////////////////////////////////
 
-// button action for encryption / descryption
+// button action for encryption / decryption
 void on_enc_action_btn_clicked(GtkButton *b) {
 
     if(!_isDecryption) {
@@ -83,6 +102,40 @@ void on_enc_action_btn_clicked(GtkButton *b) {
         encrypted_txt = encrypt_caesar(text, key);
 
         gtk_text_buffer_set_text(buffer_res, encrypted_txt, -1);
+    }
+
+}
+
+// reverting the actions for the algorithm (from enc to dec)
+void on_revert_caesar_btn_clicked(GtkButton *b) {
+
+    // empty text 
+    gchar *empty_text;
+    empty_text = "";
+
+    // emptying the text 
+    GtkTextBuffer *buffer = gtk_text_view_get_buffer (encr_txt);
+    GtkTextBuffer *buffer_res = gtk_text_view_get_buffer (decr_txt);
+    GtkTextBuffer *buffer_key = gtk_text_view_get_buffer (key_length);
+    gtk_text_buffer_set_text(buffer, empty_text, -1);
+    gtk_text_buffer_set_text(buffer_res, empty_text, -1);
+    gtk_text_buffer_set_text(buffer_key, empty_text, -1);
+
+    if(!_isDecryption) {
+        
+        // setting labels for the decryption explanation
+        gtk_label_set_markup (descr_txt_label, LBL_TXT_2_DEC);
+        gtk_button_set_label(action_btn, LBL_BTN_2_DEC);
+
+        _isDecryption = True;
+    }
+    else {
+
+        // setting labels for the encryption explanation
+        gtk_label_set_markup (descr_txt_label, LBL_TXT_1_ENC);
+        gtk_button_set_label(action_btn, LBL_BTN_1_ENC);
+
+        _isDecryption = False;
     }
 
 }
